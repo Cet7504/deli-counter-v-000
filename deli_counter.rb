986a4b6e90bfc1fe55cbd0169f17ad9c638c67ed
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-04-24 02:09:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
    if katz_deli.length == 0
      puts "The line is currently empty."
    else
-     intro = "The line is currently:"
+     new_array = "The line is currently:"
      katz_deli.each_with_index do |name, index|
-     intro << " #{index + 1}. #{name}"
+     new_array << " #{index + 1}. #{name}"
    end
      puts intro
    end
@@ -17,11 +17,12 @@ def line(katz_deli)
    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
  end
 
- def now_serving(other_deli)
-   if other_deli != []
-     puts "Currently serving #{other_deli[0]}."
-     other_deli.shift
-   else
+ def now_serving(katz_deli)
+   if katz_deli.count == 0 
      puts "There is nobody waiting to be served!"
-   end
+   else 
+   puts "Currently serving #{katz_deli[0]}."
+   katz_deli.shift
  end
+ end
+ 
